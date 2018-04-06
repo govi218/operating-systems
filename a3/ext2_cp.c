1d68fe8460257cef844ef5d956d64552f7abee97
VinoshanT <tvinoshan@hotmail.com>
2018-04-06 18:43:08 -0400

    compile cp

--- a3/ext2_cp.c
+++ a3/ext2_cp.c
@@ -13,7 +13,7 @@ int do_cp(char *ext2_disk_name, char *osdir, char *imgdir) {
     }
 
     // check if file exists on os exists
-    FILE fp*;
+    FILE *fp;
     fp = fopen(osdir, "r");
     if (fp == NULL){
         printf("File does not exist");
@@ -45,37 +45,38 @@ int do_cp(char *ext2_disk_name, char *osdir, char *imgdir) {
     // check if enough space to add new file
 
     unsigned int freeInodeNum = next_inode(disk);
-    update_inode_bmp(disk, freeInodeNum, "a");
+    update_inode_bmp(disk, freeInodeNum, 'a');
 
     // create new i_node for file
     struct ext2_inode *new_inode = (struct ext2_inode *)(disk + (EXT2_BLOCK_SIZE) + (EXT2_INODE_SIZE*freeInodeNum));
 
     new_inode -> i_mode = EXT2_S_IFREG;
     new_inode  -> i_size = file_length;
-    int blocks = (file_length - 1)/(EXT2_BLOCK_SIZE) + 1
+    int blocks = (file_length - 1)/(EXT2_BLOCK_SIZE) + 1;
     new_inode -> i_blocks  = 2*(blocks);
-    new_inode -> i_date = 0;
+    new_inode -> i_ctime = 0;
     new_inode -> i_links_count = 1;
     // add new blocks
     for (int i = 0; i< blocks; i++){
         new_inode -> i_block[i] = next_block(disk);
-        update_block_bmp(disk, new_inode -> i_block[i], mod);
+        update_block_bmp(disk, new_inode -> i_block[i], 'a');
     }
     // copy file contents into blocks
     int num = blocks;
     if (num > 12){
         num = 12;
     }
-    for (int i = 0; i < num; i++){
+    int i;
+    for (i = 0; i < num; i++){
         void *block;
         block = (void *)(disk + (new_inode->i_block[i] * EXT2_BLOCK_SIZE));
-        fread(block, sizeof(char), EXT2_BLOCK_SIZE/sizeof, fp);
+        fread(block, sizeof(char), EXT2_BLOCK_SIZE/sizeof(char), fp);
     }
 
     // new dir
-    ext2_dir_entry_2 * curr_dir_entry;
+    struct ext2_dir_entry_2 * curr_dir_entry;
     for (int i = 0; i < (pd_inode -> i_blocks)/2 && i<12; i ++){
-        curr_dir_entry = (ext2_dir_entry_2 *)(disk + (EXT2_BLOCK_SIZE*(pd_inode->i_block[i])));
+        curr_dir_entry = (struct ext2_dir_entry_2 *)(disk + (EXT2_BLOCK_SIZE*(pd_inode->i_block[i])));
     }
 
     if (pd_inode->i_block[i]){
@@ -86,17 +87,17 @@ int do_cp(char *ext2_disk_name, char *osdir, char *imgdir) {
         }
 
         int prev_dir_size = sizeof(struct ext2_dir_entry_2) + curr_dir_entry->name_len + (curr_dir_entry->name_len%4);
-        ext2_dir_entry_2 * new_dir_entry;
+        struct ext2_dir_entry_2 * new_dir_entry;
         new_dir_entry = (struct ext2_dir_entry_2 *)((char *)curr_dir_entry + prev_dir_size);
         new_dir_entry -> inode = freeInodeNum;
         new_dir_entry -> name_len = strlen(file_name);
-        new_dir_entry -> file_type = EXT2_S_IFREG;
-        new_dir_entry -> name = file_name;
-        new_dir_entry -> rec_len = 
+        new_dir_entry -> file_type = EXT2_FT_DIR;
+        strncpy(new_dir_entry -> name, file_name, strlen(file_name));
+        new_dir_entry -> rec_len = curr_dir_entry->rec_len - prev_dir_size;
     }
 
     fclose(fp);
-    
+    return 0;
 }
 
 int main(int argc, char **argv) {
--- a3/ext2_helpers.h
+++ a3/ext2_helpers.h
@@ -5,7 +5,10 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-unsigned int next_inode(char* disk);
-unsigned int next_block(char* disk);
+unsigned int next_inode(unsigned char* disk);
+unsigned int next_block(unsigned char* disk);
 struct ext2_inode* search_for_subdirectory(unsigned char* disk, struct ext2_inode *cur_inode, char *sub_dir_name, unsigned int inode_tbl_size);
 struct ext2_inode* go_to_destination(unsigned char* disk, char *path);
+void getParentDirectory(char *parentDir, char *imgFilePath);
+void update_inode_bmp(unsigned char* disk, unsigned int inode_num, char mod);
+void update_block_bmp(unsigned char* disk, unsigned int block_num, char mod);
