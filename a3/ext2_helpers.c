35b726dbec4a2f2906a35119ea378f1959925670
Govind Mohan <govi218mu@gmail.com>
2018-04-06 13:03:37 -0400

    added bmp update

--- a3/ext2_helpers.c
+++ a3/ext2_helpers.c
@@ -33,6 +33,24 @@ unsigned int next_block(char* disk) {
     return -ENOMEM;
 }
 
+void update_inode_bmp(unsigned char* disk, unsigned int inode_num, char mod) {
+    struct ext2_super_block* sb = (struct ext2_super_block*)(disk + EXT2_BLOCK_SIZE);
+    struct ext2_group_desc* gd = (struct ext2_group_desc*)(disk + 2 * EXT2_BLOCK_SIZE);
+    
+    sb->s_free_inodes_count ++;
+    char* inode_bmp = (char*) (disk + gd->bg_inode_bitmap * EXT2_BLOCK_SIZE);
+   
+    if(mod == 'a') {
+        inode_bmp[(inode_num - 1) / 8] = inode_bmp[(inode_num - 1) / 8] | (1 << (inode_num - 1) % 8);
+    } else if(mod == 'd') {
+        inode_bmp[(inode_num - 1) / 8] = inode_bmp[(inode_num - 1) / 8] & ~(1 << (inode_num - 1) % 8);
+    } else {
+        printf("FAILURE\n");
+        exit(EXIT_FAILURE);
+    }
+    
+}
+
 /*
 Searches the directory for a given filename. If it doesnt exist NULL is returned
 */
